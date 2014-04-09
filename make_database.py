e4ad2863236cd36e5860f1d17a06ca05e30216d5
Brian Hines <brian@projectweekend.net>
2014-04-09 11:16:22 -0500

    Store more stuff about songs in the queue

--- make_database.py
+++ make_database.py
@@ -5,7 +5,14 @@ CREATE_SONG_QUEUE = '''
 CREATE TABLE IF NOT EXISTS 
 jukebox_song_queue (
     spotify_uri TEXT,
-    has_played INTEGER DEFAULT 0
+    has_played INTEGER DEFAULT 0,
+    name TEXT,
+    artist_name TEXT,
+    artist_uri TEXT,
+    artist_image TEXT,
+    album_name TEXT,
+    album_uri TEXT,
+    album_image TEXT
 );
 '''
 
