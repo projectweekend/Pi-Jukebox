7d81ae94fb88fc6fc5f84a560ba40cb752a09afe
Brian Hines <brian@projectweekend.net>
2014-04-09 06:19:18 -0500

    Add route to add track to play queue

--- web_app/app.js
+++ web_app/app.js
@@ -9,7 +9,8 @@ var express = require('express'),
   http = require('http'),
   path = require('path'),
   sqlite3 = require('sqlite3'),
-  spotifyAPI = require('./routes/spotifyAPI');
+  spotifyAPI = require('./routes/spotifyAPI'),
+  jukeboxAPI = require('./routes/jukeboxAPI');
 
 var app = module.exports = express();
 var server = require('http').createServer(app);
@@ -56,6 +57,7 @@ app.get('/api/name', api.name);
 app.get('/api/search/spotify/track', spotifyAPI.trackSearch);
 app.get('/api/search/spotify/album', spotifyAPI.albumSearch);
 app.get('/api/search/spotify/artist', spotifyAPI.artistSearch);
+app.post('/api/jukebox/add', jukeboxAPI.addTrackToQueue);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
---
+++ web_app/jukeboxAPI.js
@@ -0,0 +1,26 @@
+var sqlite3 = require('sqlite3');
+
+
+var errorHandler = function ( err, res ) {
+    console.log( err );
+    res.send( 500 );
+};
+
+
+exports.addTrackToQueue = function ( req, res ) {
+
+    var db = new sqlite3.Database( '../jukebox.db' );
+
+    var uri = req.body.uri;
+
+    var sql = db.prepare( "INSERT INTO jukebox_song_queue VALUES (?, 0)" );
+    try {
+        sql.run( uri );
+        sql.finalize();
+    } catch ( e ) {
+        return errorHandler( e, res );
+    }
+    db.close();
+    return res.json( { track_added: uri } );
+
+};
