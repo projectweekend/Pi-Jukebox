dbcfec652238bda885e6d077ef3fad801491d011
Brian Hines <brian@projectweekend.net>
2014-04-15 06:17:04 -0500

    Stub out including LastFM data in the URI lookup

--- web_app/routes/lookupAPI.js
+++ web_app/routes/lookupAPI.js
@@ -8,6 +8,12 @@ var errorHandler = function ( err, res ) {
 };
 
 
+var LastFM = new LastfmAPI({
+    api_key : process.env.LAST_FM_KEY,
+    secret : process.env.LAST_FM_SECRET
+});
+
+
 exports.byURI = function ( req, res ) {
 
     var uri = req.query.uri;
@@ -22,6 +28,18 @@ exports.byURI = function ( req, res ) {
         if ( err ) {
             return errorHandler( err, res );
         }
+        // start building the output object
+        var output = {
+            artist: {
+                name: data.artist.name,
+                href: data.artist.href
+            }
+        };
+
+        // Get more interesting data from lastFM
+        // TODO: use Async library here to make calls to external services
+        // LastFM.artist.getInfo();
+
         return res.json( data );
     } );
 
